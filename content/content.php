bce0f027995171ef4210d5519fb3753f90adc1db
Jason Conroy <jason@findingsimple.com>
2015-01-26 21:26:27 +1100

    Revert

--- content/content.php
+++ content/content.php
@@ -6,39 +6,7 @@ if ( ! empty( $_SERVER['SCRIPT_FILENAME'] ) && basename( __FILE__ ) == basename(
     die ( 'You do not have sufficient permissions to access this page!' );
 }
 
-if ( is_home() ) { ?>
-
-<article <?php hybrid_attr( 'post' ); ?>>
-
-	<header class="entry-header page-header">
-		<h1 <?php hybrid_attr( 'entry-title' ); ?>><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h1>
-	</header><!-- .entry-header -->
-
-	<div class="entry-byline">
-		Published by <span <?php hybrid_attr( 'entry-author' ); ?>><?php the_author_posts_link(); ?></span> on <time <?php hybrid_attr( 'entry-published' ); ?>><?php echo get_the_date(); ?></time> <?php maybe_comments_link(); ?> <?php edit_post_link( 'Edit This', '| ' ); ?>
-	</div>
-
-	<div <?php hybrid_attr( 'entry-content' ); ?>>
-		<?php the_content(); ?>
-		<?php 
-		if ( function_exists( 'wp_link_pages_extended' ) ) {
-			wp_link_pages_extended( array( 'before' => '<div class="pagination-wrapper pagination-centered"><ul class="pagination">', 'after' => '</ul></div>', 'before_page' => '<li>', 'before_current_page' => '<li class="active">', 'after_page' => '</li>'  ) ); 
-		} else {
-			wp_link_pages();
-		}
-		?>
-	</div><!-- .entry-content -->
-
-	<footer class="entry-footer">
-		<div class="entry-meta">
-			<?php hybrid_post_terms( array( 'taxonomy' => 'category', 'text' => __( 'Posted in %s', hybrid_get_parent_textdomain() ) ) ); ?> <?php hybrid_post_terms( array( 'taxonomy' => 'post_tag', 'text' => __( 'Tagged %s', hybrid_get_parent_textdomain() ), 'before' => ' | ' ) ); ?>
-			<time class="updated" style="display:none;" datetime="<?php echo get_the_modified_time( 'Y-m-d\TH:i:sP' ); ?>" title="<?php echo get_the_modified_time( esc_attr__( 'l, F jS, Y, g:i a', hybrid_get_parent_textdomain() ) ); ?>"><?php echo get_the_modified_time( get_option( 'date_format' ) ) ?></time>
-		</div>
-	</footer><!-- .entry-footer -->
-
-</article><!-- .hentry -->
-
-<?php } elseif ( is_singular() ) { ?>
+if ( is_singular() ) { ?>
 
 <article <?php hybrid_attr( 'post' ); ?>>
 
